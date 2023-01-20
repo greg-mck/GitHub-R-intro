c93dfcf52c99deacbdc388e5266554c627e8c8b2
greg-mck <gwmckittrick@gmail.com>
2023-01-20 09:43:27 -0700

    worked on lesson 2 a bit more before class

--- Week1_lesson2.R
+++ Week1_lesson2.R
@@ -13,5 +13,28 @@ counties <- counties(state = "CO")
 
 roads <- roads(state = "CO", county = "Larimer")
 
-#setup tmap mode to iterative 
+#setup tmap mode to iteractive 
 
+tmap_mode("view")
+
+qtm(counties)
+
+tm_shape(counties)+
+  tm_polygons()
+
+qtm(counties)+
+    qtm(roads)
+
+
+#look at what data type 
+
+class(counties)
+
+#counties object is fata frame and a sf.
+#sf is useful for ploptting this stuff
+
+#convert data frame of lat long coordinates to sf 
+
+poudre_points <- data.frame(name = c("Mishawaka", "Rustic", "Blue Lake Trailhead"),
+                            long = c(-105.35634, -105.58159, -105.85563),
+                            lat = c(40.68752, 40.69687, 40.57960))
